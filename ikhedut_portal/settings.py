7e3417dc7b289d299a68d022d8bc9aa0f127c3aa
princeantala45 <princeantala45@gmail.com>
2026-02-26 14:43:07 +0530

    -

--- ikhedut_portal/settings.py
+++ ikhedut_portal/settings.py
@@ -238,15 +238,26 @@ LOGIN_URL = "login"
 
 # AUTH_USER_MODEL = 'ikhedut.User'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'infoikhedutportal@gmail.com'
-EMAIL_HOST_PASSWORD = 'ygdg unzo sxik pely'
+# # EMAIL_HOST_USER = 'infoikhedutportal@gmail.com'
+# # EMAIL_HOST_PASSWORD = 'ygdg unzo sxik pely'
 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 # EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
