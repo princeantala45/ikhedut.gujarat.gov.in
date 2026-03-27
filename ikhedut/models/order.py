d5083f2c394001ab5e7d9d64736b1592e9d7440e
Prince Antala <princeantala45@gmail.com>
2026-03-27 15:59:29 +0530

    general code review

--- ikhedut/models/order.py
+++ ikhedut/models/order.py
@@ -77,4 +77,4 @@ class OrderItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return f"Order {self.order.id} Item"
+        return f"Order {self.order.id} Item" # type: ignore
--- ikhedut_portal/settings.py
+++ ikhedut_portal/settings.py
@@ -31,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "adminsortable2",
-    "ckeditor",
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
